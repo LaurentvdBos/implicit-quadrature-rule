336cea1e2fa231337c45290b7d2a7300abe29c14
Laurent van den Bos <laurentvdbos@outlook.com>
2019-05-01 17:28:39 +0200

    Remove QR decomposition code
    We are going to use LAPACKE to do the QR decomposition. So first the
    matrix code is rewritten such that it stores the matrices row-major.
    Then LAPACK can be used to determine a null space by means of a QR
    decomposition. Since we need to determine a QR decomposition of A' for
    that, the column-major ordering of LAPACK will help us.

--- implquad.c
+++ implquad.c
@@ -8,35 +8,5 @@ int main()
 {
 	srand(time(0));
 
-	struct matrix *mat = matrix_malloc(10, 11);
-	
-	double a[mat->n*mat->m];
-	for (int i = 0; i < mat->n*mat->m; i++) {
-		a[i] = (double)rand()/(double)RAND_MAX*2.-1.;
-	}
-	matrix_set(mat, a);
-
-	printf("a:\n");
-	matrix_fprintf(stdout, mat, "%lf");
-
-	struct matrix *q = matrix_malloc(mat->n, mat->n);
-	struct matrix *r = matrix_malloc(mat->n, mat->m);
-	struct matrix *qr = matrix_malloc(mat->n, mat->m);
-
-	matrix_qr(mat, q, r);
-	printf("\nq:\n");
-	matrix_fprintf(stdout, q, "%lf");
-	printf("\nr:\n");
-	matrix_fprintf(stdout, r, "%lf");
-
-	matrix_mul(qr, q, r);
-	printf("\nq*r:\n");
-	matrix_fprintf(stdout, qr, "%lf");
-
-	matrix_free(mat);
-	matrix_free(q);
-	matrix_free(r);
-	matrix_free(qr);
-
 	return 0;
 }
--- matrix.h
+++ matrix.h
@@ -15,6 +15,5 @@ void matrix_set(struct matrix *mat, const double *a);
 void matrix_fprintf(FILE *f, const struct matrix *mat, const char *fmt);
 void matrix_mul(struct matrix *mat, const struct matrix *b, const struct matrix *c);
 void matrix_multt(struct matrix *mat, const struct matrix *b, const struct matrix *c);
-void matrix_householder(struct matrix *mat, struct matrix *q, int k);
 
 #endif
