bf619d11c69017cd981d47785eed458cd8bd72b3
Laurent van den Bos <laurentvdbos@outlook.com>
2019-10-31 15:11:39 +0100

    Make variable n a constant

--- isort.c
+++ isort.c
@@ -1,6 +1,6 @@
 // A basic implementation of insertion sort. It sorts an array a with n
 // integers.
-void isort(int *a, int n)
+void isort(int *a, const int n)
 {
 	for (int i = 1; i < n; i++) {
 		for (int j = i; j > 0 && a[j] < a[j-1]; j--) {
--- isort.h
+++ isort.h
@@ -4,6 +4,6 @@
 // The lists that we need to sort are quite short (length d) and often already
 // approximately sorted. Quicksort is relatively slow to work with these lists,
 // so we use an insertion sort.
-void isort(int *a, int n);
+void isort(int *a, const int n);
 
 #endif
